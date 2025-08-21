3f0ecc7c810c953c0e132352cbfbd9ce42427128
khaledayman <khaledaymanelalmy2006@gmail.com>
2025-08-21 21:10:16 +0300

    finish changes

--- Task 6/js/script3.js
+++ Task 6/js/script3.js
@@ -1,47 +1,38 @@
 const container = document.getElementById("productsContainer");
 
-// Fetch API data
 fetch("https://fakestoreapi.com/products")
 .then(res => res.json())
 .then(products => {
     products.forEach(product => {
-      // Create card
         const card = document.createElement("div");
         card.className = "card";
 
-      // Product image
-const img = document.createElement("img");
-img.src = product.image;
+        const img = document.createElement("img");
+        img.src = product.image;
 
-      // Title
-const title = document.createElement("h3");
-title.textContent = product.title;
+        const title = document.createElement("h3");
+        title.textContent = product.title;
 
-      // Category
-const category = document.createElement("p");
-category.textContent = product.category;
+        const category = document.createElement("p");
+        category.textContent = product.category;
 
-      // Price + Rating
-const priceRating = document.createElement("div");
-priceRating.className = "price-rating";
-priceRating.innerHTML = `<span>${product.price} $</span> <span>Rating : ${product.rating.rate}</span>`;
+        const priceRating = document.createElement("div");
+        priceRating.className = "price-rating";
+        priceRating.innerHTML = `<span>${product.price} $</span> <span>Rating : ${product.rating.rate}</span>`;
 
-      // Button
-const btn = document.createElement("button");
-btn.textContent = "Show Details";
-btn.addEventListener("click", () => {
-        alert(product.description);
-});
+        const btn = document.createElement("button");
+        btn.textContent = "Show Details";
+        btn.addEventListener("click", () => {
+            alert(product.description);
+        });
 
-      // Append to card
 card.appendChild(img);
 card.appendChild(title);
 card.appendChild(category);
 card.appendChild(priceRating);
 card.appendChild(btn);
 
-      // Append to container
 container.appendChild(card);
-});
+    });
 })
-.catch(err => console.error("Error fetching products:", err));
+    .catch(err => console.error("Error fetching products:", err));
